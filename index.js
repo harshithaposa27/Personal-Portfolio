efa515df5674aac111e948633525da4521b356d6
harshithaposa27 <harshithaposa.career@gmail.com>
2025-09-14 13:21:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const greeting = document.getElementById("greeting");
+const hour = new Date().getHours();
+
+if (hour < 12) {
+    greeting.textContent = "☀️ Good Morning!";
+} else if (hour < 18) {
+    greeting.textContent = "🌤️ Good Afternoon!";
+} else {
+    greeting.textContent = "🌙 Good Evening!";
+}
